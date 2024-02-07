aabbdf0ed616137a860c9529f4dc09dc8b0123b3
Aayush <aceraayush@gmail.com>
2024-02-07 16:58:52 +0545

    templates

--- home/views.py
+++ home/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.views import View
-
+from .models import *
 
 class BaseView(View):
     views = {}
-
+    views['categories'] = Category.objects.all()
+    views['brands'] = Brand.objects.all()
+    views['contacts'] = ContactInfo.objects.all()
 
 class HomeView(BaseView):
     def get(self,request):
-
-        return render(request,'index.html')
+        self.views
+        self.views['sliders'] = Slider.objects.all()
+        self.views['ads'] = Ad.objects.all()
+        self.views['products'] = Product.objects.all()
+        return render(request,'index.html',self.views)
